a25b63867a0dd117b37b3b8e81ac809878c2009b
greenharvest <145071038+greenharvest@users.noreply.github.com>
2023-09-14 19:13:03 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Función para manejar la suscripción
+document.getElementById("suscribirse").addEventListener("click", function () {
+    // Aquí puedes agregar la lógica para manejar la suscripción
+    alert("Gracias por suscribirte a GreenHarvest. ¡Bienvenido a nuestra comunidad!");
+});
+
+// Función para cargar el calendario lunar interactivo
+function cargarCalendarioLunar() {
+    // Aquí puedes agregar la lógica para cargar el calendario lunar interactivo
+    alert("Calendario lunar cargado.");
+}
+
+// Función para cargar la parcela en el metaverso de granja
+function cargarParcelaMetaverso() {
+    // Aquí puedes agregar la lógica para cargar la parcela en el metaverso de granja
+    alert("Parcela en el metaverso cargada. ¡Comienza a cultivar en línea!");
+}
+
+// Función para cargar el perfil de la huerta del usuario
+function cargarPerfilHuerta() {
+    // Aquí puedes agregar la lógica para cargar el perfil de la huerta del usuario
+    alert("Perfil de tu huerta cargado. ¡Personaliza tu espacio de cultivo!");
+}
+
+// Lógica adicional de tu aplicación puede ir aquí
