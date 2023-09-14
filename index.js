ca7858222713a3a123f76b2d9af6dd9046156e54
greenharvest <145071038+greenharvest@users.noreply.github.com>
2023-09-14 20:10:35 -0300

    Update index.html

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
-// Función para manejar la suscripción
-document.getElementById("suscribirse").addEventListener("click", function () {
-    // Aquí puedes agregar la lógica para manejar la suscripción
-    alert("Gracias por suscribirte a GreenHarvest. ¡Bienvenido a nuestra comunidad!");
-});
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GreenHarvest - Tu Jardín en Línea</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="background.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>GreenHarvest</h1>
+            <p>Tu Jardín en Línea</p>
+        </header>
 
-// Función para cargar el calendario lunar interactivo
-function cargarCalendarioLunar() {
-    // Aquí puedes agregar la lógica para cargar el calendario lunar interactivo
-    alert("Calendario lunar cargado.");
-}
+        <section id="informacion">
+            <h2>Bienvenido a GreenHarvest</h2>
+            <p>Descubre consejos de jardinería, información sobre plantas y mucho más para cultivar un jardín próspero y sostenible.</p>
+        </section>
 
-// Función para cargar la parcela en el metaverso de granja
-function cargarParcelaMetaverso() {
-    // Aquí puedes agregar la lógica para cargar la parcela en el metaverso de granja
-    alert("Parcela en el metaverso cargada. ¡Comienza a cultivar en línea!");
-}
+        <section id="suscripcion">
+            <h2>Únete a nuestra Comunidad</h2>
+            <p>Obtén acceso completo a nuestras recomendaciones de cultivo, calendario lunar y parcela en el metaverso de granja.</p>
+            <div class="suscripcion-options">
+                <div class="subscription-option">
+                    <h3>Plan Mensual</h3>
+                    <p>Acceso ilimitado por solo $9.99 al mes</p>
+                    <button id="suscribirse-mensual">¡Suscríbete Ahora!</button>
+                </div>
+                <div class="subscription-option">
+                    <h3>Plan Anual</h3>
+                    <p>Ahorra un 20% con la suscripción anual</p>
+                    <button id="suscribirse-anual">¡Suscríbete Ahora!</button>
+                </div>
+            </div>
+        </section>
 
-// Función para cargar el perfil de la huerta del usuario
-function cargarPerfilHuerta() {
-    // Aquí puedes agregar la lógica para cargar el perfil de la huerta del usuario
-    alert("Perfil de tu huerta cargado. ¡Personaliza tu espacio de cultivo!");
-}
+        <section id="calendario-lunar">
+            <h2>Calendario Lunar</h2>
+            <p>Descubre cuándo es el momento perfecto para plantar y cosechar tus cultivos favoritos.</p>
+            <div id="calendario">
+                <!-- Aquí podría ir el calendario lunar interactivo -->
+            </div>
+        </section>
 
-// Lógica adicional de tu aplicación puede ir aquí
+        <section id="metaverso">
+            <h2>Metaverso de Granja</h2>
+            <p>Cultiva tu propio espacio virtual en nuestro metaverso de granja. ¡Personaliza tu huerta y observa cómo crecen tus plantas!</p>
+            <div id="parcela">
+                <!-- Aquí podría ir la parcela interactiva del metaverso -->
+            </div>
+            <div id="perfil-huerta">
+                <!-- Aquí podría ir el perfil de la huerta del usuario -->
+            </div>
+        </section>
+
+        <footer>
+            <p>Contacto: info@greenharvest.com</p>
+            <p>Síguenos en <a href="#">Facebook</a> | <a href="#">Twitter</a> | <a href="#">Instagram</a></p>
+        </footer>
+    </div>
+    <script src="index.js"></script>
+</body>
+</html>
